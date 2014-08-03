f9de7eb502cb582d7c0d9d32ee312c660a158b73
Colden Cullen <ColdenCullen@gmail.com>
2014-08-02 20:29:41 -0700

    Implemented DuckPointer

--- source/quack/pointer.d
+++ source/quack/pointer.d
@@ -5,6 +5,19 @@ module quack.pointer;
 import quack.extends, quack.mixins;
 
 /**
+ * Convience wrapper for creating a DuckPointer.
+ */
+template duck( Pointee ) if( isExtendable!Pointee )
+{
+    DuckPointer!Pointee duck( Pointer )( Pointer* p )
+    {
+        DuckPointer!Pointee ptr;
+        ptr.init!Pointer( p );
+        return ptr;
+    }
+}
+
+/**
  * Pointer to any type that `extends` Pointee.
  *
  * Params:
@@ -15,20 +28,67 @@ template DuckPointer( Pointee ) if( isExtendable!Pointee )
     struct DuckPointer
     {
     public:
+        mixin( ImplementMembers );
+
+        ~this()
+        {
+            destroyer( impl );
+        }
 
     private:
+        void* impl;
+        void function( void* ) destroyer;
 
+        void init( Pointer )( Pointer* p )
+        {
+            impl = cast(void*)p;
+            destroyer = ptr => typeid(Pointer).destroy( ptr );
+
+            // Assign pointers
+            foreach( member; __traits( allMembers, Pointee ) )
+            {
+                import std.algorithm: among;
+                static if( !member.among( "this", "~this" ) )
+                {
+                    mixin( "this." ~ member ) = &__traits( getMember, p, member );
+                }
+            }
+        }
     }
+
+    private enum ImplementMembers = {
+        string members = "";
+
+        foreach( member; __traits( allMembers, Pointee ) )
+        {
+            import std.algorithm: among;
+            static if( !member.among( "this", "~this" ) )
+            {
+                members ~= typeof( &__traits( getMember, Pointee, member ) ).stringof.replace( "function", "delegate" );
+                members ~= " " ~ member ~ ";";
+            }
+        }
+
+        return members;
+    } ();
 }
 ///
 unittest
 {
-    interface IFace1
+    struct IFace1
     {
-        int getX();
+        int getX() { return 12; }
+    }
+    struct Struct1
+    {
+        int x;
+        int getX() { return x; }
     }
 
-    DuckPointer!IFace1 ptr1;
+    Struct1 s1;
+    s1.x = 42;
+    DuckPointer!IFace1 ptr1 = duck!IFace1( &s1 );
+    assert( ptr1.getX() == 42 );
 }
 
 /**
